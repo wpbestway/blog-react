d5904acc5646811fe29a22967593c5058e8d6f84
Rachel <rachelx@myuniuni.com>
2019-08-14 21:44:12 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import './index.scss';
+
+const render = () => {
+  ReactDOM.render(
+    <div>123</div>,
+    document.querySelector('#app')
+  )
+}
+
+render()
